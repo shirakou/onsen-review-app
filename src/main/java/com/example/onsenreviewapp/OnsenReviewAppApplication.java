5740c490d958142cc811ff3ed91d882dce271b42
shirakou <koudai126126@icloud.com>
2026-08-02 17:01:11 +0900

    Initial project setup

---
+++ src/main/java/com/example/onsenreviewapp/OnsenReviewAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.onsenreviewapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnsenReviewAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnsenReviewAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/onsenreviewapp/OnsenReviewAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onsenreviewapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnsenReviewAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
