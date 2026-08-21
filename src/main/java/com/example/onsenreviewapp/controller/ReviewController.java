7bc584696ea84136c06b35c217bc1bab3daaebae
shirakou <koudai126126@icloud.com>
2026-08-21 22:11:32 +0900

    Implement review update

--- src/main/java/com/example/onsenreviewapp/controller/ReviewController.java
+++ src/main/java/com/example/onsenreviewapp/controller/ReviewController.java
@@ -6,13 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.onsenreviewapp.entity.Review;
 import com.example.onsenreviewapp.entity.User;
 import com.example.onsenreviewapp.form.ReviewForm;
 import com.example.onsenreviewapp.service.ReviewService;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class ReviewController {
@@ -59,6 +63,51 @@ public class ReviewController {
 		model.addAttribute("reviewId", reviewId);
 		
 		return "review/edit";
+		
+	}
+	
+	@PostMapping("/reviews/{reviewId}/edit")
+	public String updateReview(
+			@PathVariable Long reviewId,
+			@Valid ReviewForm reviewForm,
+			BindingResult bindingResult,
+			Authentication authenticator,
+			Model model
+			) {
+		
+		Optional<Review> targetReview = reviewService.getReviewById(reviewId);
+		
+		if (targetReview.isEmpty()) {
+			return "redirect:/";
+		}
+		
+		//Optionalから更新対象のReviewを取り出す
+		Review review = targetReview.get();
+		
+		//review投稿したユーザーのemailを取得
+		String reviewEmail = review.getUser().getEmail();
+		
+		//現在ログインしているユーザーのemailを取得
+		String email = authenticator.getName();
+		
+		//ログインしているユーザーとreview投稿したユーザーが正しいか
+		if (!reviewEmail.equals(email)) {
+			return "redirect:/onsens/" + review.getOnsen().getId();
+		}
+		
+		if (bindingResult.hasErrors()) {
+			model.addAttribute("reviewId", reviewId);
+			return "review/edit";
+		}
+		
+		review.setTitle(reviewForm.getTitle());
+		review.setRating(reviewForm.getRating());
+		review.setComment(reviewForm.getComment());
+		
+		reviewService.saveReview(review);
+		
+		
+		return "redirect:/onsens/" + review.getOnsen().getId();
 		
 	}
 }
